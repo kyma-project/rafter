cd2185d699473c3bb373c9909d8bb04fcd317e27
Mateusz Puczynski <mateusz.puczynski@sap.com>
2019-11-11 20:48:23 +0100

    Add hack/ci folder

---
+++ hack/ci/lib/deps_ver.sh
@@ -0,0 +1,6 @@
+# Upgrade binary versions in a controlled fashion
+# along with the script contents (config, flags...)
+readonly STABLE_KUBERNETES_VERSION=v1.16.2
+readonly STABLE_KIND_VERSION=v0.5.1
+readonly STABLE_HELM_VERSION=v2.16.0
+readonly CT_VERSION=v2.3.3
