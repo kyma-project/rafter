92b51bab3412d4e374c68450b38aec06f05b6dba
Mateusz Puczynski <mateusz.puczynski@sap.com>
2020-12-30 13:32:05 +0100

    revert helm version

--- hack/ci/envs.sh
+++ hack/ci/envs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-readonly STABLE_HELM_VERSION="v2.17.0"
+readonly STABLE_HELM_VERSION="v2.16.1"
 readonly STABLE_KIND_VERSION="v0.5.1"
 readonly STABLE_KUBERNETES_VERSION="v1.16.3"
 readonly STABLE_INGRESS_VERSION="1.34.2"
